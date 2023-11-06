5c4d917f25c102be77b08514dda4a8191e90f4c8
SuyoungMoon <99530280+MoonS1120@users.noreply.github.com>
2023-11-06 19:08:54 +0800

    Update merge-strings-alternately.py

--- merge-strings-alternately.py
+++ merge-strings-alternately.py
@@ -1,7 +1,12 @@
 def mergeAlternately(self, word1: str, word2: str) -> str:
+        i, j = 0, 0
         result = []
 
-        for i in range(len(word2)):
+        while i<len(word1) and j<len(word2):
             result.append(word1[i])
-            result.append(word2[i])
+            result.append(word2[j])
+            i+=1
+            j+=1
+        result.append(word1[i:])
+        result.append(word2[j:])
         return ''.join(result)
